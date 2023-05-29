6cdf5bcb041a31be7b1c4aaaed300f0e77e9dd03
22026544_TranTienAnh <126400953+tienanh1812@users.noreply.github.com>
2023-05-29 20:35:25 +0700

    Update LTexture.h

--- LTexture.h
+++ LTexture.h
@@ -1,27 +1,27 @@
 #ifndef LTEXTURE_H_
 #define LTEXTURE_H_
-using namespace std;
-#include "Game.h"
 
+#include "Game.h"
+// quản lí hình ảnh và hiển thị lên màn hình
 class LTexture
 {
 public:
 	LTexture();
 
 	~LTexture();
-
+    //giải phóng bộ nhớ và thiết lập lại các giá trị của đối tượng LTexture.
 	void Free();
-
-	bool LoadFromRenderedText(string textureText, TTF_Font* gFont, SDL_Color textColor, SDL_Renderer* gRenderer);
-
-	bool LoadFromFile(string path, SDL_Renderer *gRenderer);
-
+    //tải hình ảnh từ văn bản đã cho, sử dụng một phông chữ, màu văn bản .
+	bool LoadFromRenderedText(std::string textureText, TTF_Font* gFont, SDL_Color textColor, SDL_Renderer* gRenderer);
+    // tải hình ảnh từ tệp
+	bool LoadFromFile(std::string path, SDL_Renderer *gRenderer);
+    //hiển thị hình ảnh trên màn hình tại vị trí x, y
 	void Render(int x, int y, SDL_Renderer* gRenderer, SDL_Rect* clip = nullptr);
-
+    //chiều rộng.
 	int GetWidth();
-
+    //chiều cao.
 	int GetHeight();
-
+    
 private:
 	SDL_Texture* mTexture;
 
